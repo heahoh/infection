72a9dbbc84e68c210863cb5c415d30ec5c8ea351
borNfreee <b0rn@list.ru>
2019-02-02 01:34:00 +0300

    Added empty hello test

---
+++ tests/Process/Listener/HelloTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Infection\Tests\Process\Listener;
+
+
+use PHPUnit\Framework\TestCase;
+
+class HelloTest extends TestCase
+{
+    public function test_a()
+    {
+        $this->assertSame(1, 2);
+    }
+}
